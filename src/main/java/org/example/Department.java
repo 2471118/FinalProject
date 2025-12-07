260353469ba2c69ab0ba7ba595aa5f3414e1a154
2471118 <2471118@edu.vaniercollege.qc.ca>
2025-12-07 17:23:39 -0500

    Add department Class to Git

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+public class Department {
+    private final String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    /**
+     * Checks if the department name is valid or not
+     * @param departmentName the name of the department
+     * @return whether the department name is valid or not
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char ch = departmentName.charAt(i);
+            if (!Character.isLetter(ch) && ch != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
+
+    public void setDepartmentName(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+        } else {
+            this.departmentName = null;
+        }
+    }
+}
