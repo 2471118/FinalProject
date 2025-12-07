427c6e3b0dae42e1cb8a4e9b87fe80d5e7dda5e0
2471118 <2471118@edu.vaniercollege.qc.ca>
2025-12-07 14:26:28 -0500

    Updated Address to Git

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import lombok.*;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
     private int streetNo;
     private String street;
@@ -10,7 +16,45 @@ public class Address {
     public enum Province {
         AB, BC, MB, NB, NL, NS, ON, PE, QC, SK
     }
-    static boolean isPostalCodeValid(String postalCode) {
+
+    /**
+     * Checks if the postal code is in a valid format
+     * @param postalCode the postal code
+     * @return whether the postal code is in the correct format
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char ch = postalCode.charAt(i);
+            if (i % 2 == 0) {
+                if (!Character.isLetter(ch)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(ch)) {
+                    return false;
+                }
+            }
+        }
         return true;
+    }
+
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
     }
 }
