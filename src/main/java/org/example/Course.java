555d0a0ba5b0bb03bbadc3d02f086bf5fc0ab7c4
2471118 <2471118@edu.vaniercollege.qc.ca>
2025-12-08 12:22:55 -0500

    update Course class

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
     private String courseId;
@@ -9,5 +10,56 @@ public class Course {
     private Department department;
     private ArrayList<Assignment> assignments;
     private ArrayList<Student> registeredStudents;
+
     private static int nextId;
+
+    /**
+     * Checks if the assignment weight is valid or not
+     * @return whether the assignment weight is valid or not
+     */
+    public boolean isAssignmentWeightValid() {
+        double sum = 0;
+        for (Assignment assignment : assignments) {
+            sum += assignment.getWeight();
+        }
+        return Math.abs(sum - 100) < 0.001;
+    }
+
+    public boolean registerStudent(Student student) {
+        if (registeredStudents.contains(student)) {
+            return false;
+        }
+        registeredStudents.add(student);
+        for (Assignment assignment : assignments) {
+            assignment.getScores().add(null);
+        }
+        return true;
+    }
+
+    public int[] calcStudentsAverage() {
+        int[] averages = new int[registeredStudents.size()];
+
+        for (int i = 0; i < registeredStudents.size(); i++) {
+            double sum = 0;
+
+            for (Assignment assignment : assignments) {
+                Integer weight = assignment.getScores().get(i);
+                if (weight != null)
+                    sum += weight * assignment.getWeight() / 100;
+            }
+            averages[i] = (int) Math.round(sum);
+
+        }
+        return averages;
+    }
+
+    public boolean addAssignment(String assignmentName, double weight) {
+        Assignment assignment = new Assignment(assignmentName, weight);
+        assignments.add(assignment);
+
+        for (int i = 0; i < registeredStudents.size(); i++) {
+            assignment.addScore(null);
+        }
+        return true;
+    }
 }
