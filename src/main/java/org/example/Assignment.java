ebbe53dc9bf2b3a103793f13833f6f5669bfdfa8
2471118 <2471118@edu.vaniercollege.qc.ca>
2025-12-07 19:03:45 -0500

    Update Assignment class to Git

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+
+    private static int nextId = 1;
+
+    public double calcAssignmentAvg() {
+        double sum = 0;
+        int count = 0;
+
+        for (Integer score : scores) {
+            if (score != null) {
+                sum += score;
+                count++;
+            }
+        }
+        return count == 0 ? 0 : sum / count;
+    }
+
+    public void generateRandomScore() {
+        Random random = new Random();
+
+        for (int i = 0; i < scores.size(); i++) {
+            int randomCategory = random.nextInt(11);
+        }
+
+    }
 }
