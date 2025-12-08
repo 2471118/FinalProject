eace5dfc7c66d2e50286a322b1241bac8bec9ba5
2471118 <2471118@edu.vaniercollege.qc.ca>
2025-12-07 19:05:33 -0500

    Update Student class to Git

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,46 +1,41 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
-import java.util.List;
 
-
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Student {
-    private String name;
-    private List<Course> courses;
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
 
-    public Student(String name) {
-        this.name = name;
-        this.course = new ArrayList<>();
+    private static int nextId = 1;
+
+    public enum Gender {
+        MALE, FEMALE
     }
 
-    public void addCourse(Course course) {
-        courses.add(course);
-        course.getStudent().add(this);
-
-        public String toSimplifiedString() {
-            String message = "Student{" +
-                    "name=" + name + '\'' +
-                    ", courses=";
-
-            for (Course course : courses) {
-                message += course.toSimplifiedString() + ",";
-            }
-
-            return message;
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
         }
+        return true;
     }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                '}';
-
-        for (Course course : courses) {
-            message += course.toSimplifiedString() + ",";
-        }
-
-        return message;
-    }
+    public Student(String studentName, Gender gender, Address address, Department department) {
+            this.studentId = String.format("%06d", nextId);
+            this.studentName = studentName;
+            this.gender = gender;
+            this.address = address;
+            this.department = department;
+            this.registeredCourses = new ArrayList<>();
     }
 }
